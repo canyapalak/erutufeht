5cc3a97cd4f8d41f98a8e0a38f3a98722e94522d
canyapalak <canyapalak@gmail.com>
2023-04-14 01:07:19 +0200

    🐛 fixed a bug

--- src/components/TextPanel.js
+++ src/components/TextPanel.js
@@ -31,7 +31,7 @@ export default function TextPanel({
     return () => {
       document.body.removeEventListener("keydown", handleKeyDown);
     };
-  }, [currentPart, setStartAudio, setStartText]);
+  }, [currentPart, setStartAudio, setStartText, handleKeyDown]);
 
   return (
     <>
